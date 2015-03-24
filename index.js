5e4e197f57b11e788d844827629acc786fb4fe68
James Holmes <32bitkid@gmail.com>
2015-03-23 21:40:47 -0600

    Inject `given` context into impl

--- index.js
+++ index.js
@@ -21,14 +21,14 @@
           return {
             then: function(validator) {
               tests.push(function(impl) {
-                var actual = impl.apply(void 0, args);
+                var actual = impl.apply(self, args);
                 validator.apply(self, actual, args);
               });
               return chain;
             },
             returns: function(expected, asserter) {
               tests.push(function(impl) {
-                var actual = impl.apply(void 0, args);
+                var actual = impl.apply(self, args);
                 asserter = (asserter || __defaultAssert__);
                 asserter(actual, expected);
               });
